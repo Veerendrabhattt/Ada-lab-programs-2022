f4060e9a62bf73d6fa22d03480bae5db59cea8f5
Veerendrabhattt <159219661+Veerendrabhattt@users.noreply.github.com>
2024-08-09 13:30:16 +0530

    Topological.c

--- Topological.c
+++ Topological.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
-#include<stdlib.h>
-int indegree[10],cost[10][10],n;
+int cost[10][10],indegree[10],n,i,j,k,count;
 void cal()
 {
-	int i,j;
 	for(i=0;i<n;i++)
 	{
 		indegree[i]=0;
 		for(j=0;j<n;j++)
-		 indegree[i]+=cost[j][i];
+	        indegree[i]+=cost[j][i];
 	}
 }
 void sremoval()
 {
 	int removal[10]={0};
-	int i,k,count;
-	printf("sprting order\n");
+	printf("topo order:");
 	for(count=0;count<n;count++)
 	{
 		cal();
@@ -26,24 +23,22 @@ void sremoval()
 		}
 		if(i==n)
 		{
-			printf("graph is cyclic no sol\n");
+			printf("graph is cyclic");
 			return;
 		}
 		printf("%d\t",i);
 		removal[i]=1;
 		for(k=0;k<n;k++)
 		 cost[i][k]=0;
-	}
-	printf("above is the correct sol\n");
+}
 }
 int main()
 {
-	int i,j;
-	printf("enter no. of verticces\n");
+	printf("enter no of vertices:");
 	scanf("%d",&n);
-	printf("enter matrix\n");
+	printf("enter matrix");
 	for(i=0;i<n;i++)
-	 for(j=0;j<n;j++)
-	  scanf("%d",&cost[i][j]);
-	sremoval();  
+	for(j=0;j<n;j++)
+	scanf("%d",&cost[i][j]);
+	sremoval();
 }
